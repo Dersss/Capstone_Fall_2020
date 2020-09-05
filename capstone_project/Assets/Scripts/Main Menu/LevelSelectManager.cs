69c9b5186e577f85a50847906fd0010f810b6d87
gilbertcommaandrew <andrewgilbert165@gmail.com>
2020-09-05 15:26:50 -0400

    Added Main Menu Scene and implemented basic funcionality

---
+++ capstone_project/Assets/Scripts/Main Menu/LevelSelectManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectManager : MonoBehaviour
+{
+    public GameObject ListObject, LevelButtonPrefab;
+    private GameObject listViewportObject, listContentObject;
+    private float minimumContentHeight, buttonHeight, buttonWidth;
+    private List<GameObject> levelButtons = new List<GameObject>();
+
+    void Start()
+    {
+        listViewportObject = ListObject.transform.GetChild(0).gameObject;
+        listContentObject = listViewportObject.transform.GetChild(0).gameObject;
+
+        minimumContentHeight = listViewportObject.GetComponent<RectTransform>().rect.height;
+        buttonHeight = LevelButtonPrefab.GetComponent<RectTransform>().rect.height;
+        buttonWidth = LevelButtonPrefab.GetComponent<RectTransform>().rect.width;
+    }
+
+    //eventually data-driven, called when level select is activated 
+    public void PopulateLevelList()
+    {
+        //runs Start if it hasn't been run yet, in the case of the menu manager calling this before it runs
+        if(listViewportObject == null)
+        {
+            Start();
+        }
+
+        //import list of levels, probably from XML or something
+        //for now, sample data
+        int count;
+        for (count = 0; count < 4; count++)
+        {
+            levelButtons.Add(Instantiate(LevelButtonPrefab, listContentObject.transform));
+            levelButtons[count].transform.localPosition = new Vector3(buttonWidth/2, -buttonHeight/2 + (count*-buttonHeight), 0);
+            levelButtons[count].GetComponentInChildren<UnityEngine.UI.Text>().text = "Level " + count;
+            //levelButtons[count].GetComponentInChildren<UnityEngine.UI.Image>().sprite = 
+            //set button event to right scene
+        }
+
+        if(count*buttonHeight > minimumContentHeight)
+        {
+            SetContentHeight(count*buttonHeight);
+        } else
+        {
+            SetContentHeight(minimumContentHeight);
+        }
+    }
+
+
+    //called when level select is deactivated
+    public void ResetLevelList()
+    {
+        for (int i = 0; i < levelButtons.Count;i++)
+        {
+            Destroy(levelButtons[i]);
+        }
+        levelButtons.Clear();
+        SetContentHeight(minimumContentHeight);
+    }
+
+    private void SetContentHeight(float height)
+    {
+        listContentObject.GetComponent<RectTransform>().sizeDelta = new Vector2(listContentObject.GetComponent<RectTransform>().sizeDelta.x, height);
+    }
+}
---
+++ capstone_project/Assets/Scripts/Main Menu/MainMenuStateManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuStateManager : MonoBehaviour
+{
+    public GameObject MainStateObject, LevelSelectStateObject, OptionsStateObject;
+    private AudioSource buttonAudioSource;
+
+    void Start()
+    {
+        buttonAudioSource = gameObject.GetComponent<AudioSource>();
+        MainStateObject.SetActive(true);
+        LevelSelectStateObject.SetActive(false);
+        OptionsStateObject.SetActive(false);
+    }
+
+    public void SwitchToLevelSelect()
+    {
+        MainStateObject.SetActive(false);
+        LevelSelectStateObject.SetActive(true);
+        LevelSelectStateObject.GetComponentInChildren<LevelSelectManager>().PopulateLevelList();
+        playButtonAudio();
+    }
+
+    public void BackToMainFromLevelSelect()
+    {
+        LevelSelectStateObject.SetActive(false);
+        MainStateObject.SetActive(true);
+        LevelSelectStateObject.GetComponentInChildren<LevelSelectManager>().ResetLevelList();
+        playButtonAudio();
+    }
+    public void SwitchToOptions()
+    {
+        MainStateObject.SetActive(false);
+        OptionsStateObject.SetActive(true);
+        playButtonAudio();
+    }
+    public void BackToMainFromOptions()
+    {
+        OptionsStateObject.SetActive(false);
+        MainStateObject.SetActive(true);
+        playButtonAudio();
+    }
+
+    public void QuitButtonFunction()
+    {
+        playButtonAudio();
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    private void playButtonAudio()
+    {
+        buttonAudioSource.Play();
+    }
+}
