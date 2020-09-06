033075fb71c82f6be4ed51a364e4311e1295ac55
gilbertcommaandrew <andrewgilbert165@gmail.com>
2020-09-06 15:19:35 -0400

    Added level loading using build settings index values

---
+++ capstone_project/Assets/Scripts/Main Menu/LevelSelectButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectButton : MonoBehaviour
+{
+    public int SceneBuildIndex;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneBuildIndex);
+    }
+}
