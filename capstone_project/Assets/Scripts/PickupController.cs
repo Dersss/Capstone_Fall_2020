1255990243ca62dbf813abbc60ba9b07d9d4b143
Andrew Schneider <schneider.911@osu.edu>
2020-09-16 16:26:20 -0400

    wtf2

---
+++ capstone_project/Assets/Scripts/PickupController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+    float pickUpRange = 5f;
+    int layerMask;
+    GameObject gameController;
+
+    GameObject primaryWep, secondaryWep, currentWep;
+
+    ItemDatabase database;
+    PlayerInventory inventory;
+    Camera cam;
+
+    int gunIndex = 0;
+
+    void Start() {
+        gameController = GameObject.FindWithTag("gc");
+        database = gameController.GetComponent<ItemDatabase>();
+        inventory = gameController.GetComponent<PlayerInventory>();
+        layerMask = LayerMask.GetMask("Pickup");
+        cam = GetComponent<Camera>();
+        inventory.weaponSlot[1].gameObject.SetActive(true);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            CreateGun();
+        }
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, pickUpRange, layerMask)) {
+            if (Input.GetKeyDown(KeyCode.G)) {
+                Vector3 rot = new Vector3(0,180,0);
+                GameObject newWep = Instantiate(database.weapons[0].weaponObj, inventory.weaponSlot[0].gameObject.transform.position,  Quaternion.Euler(rot));
+                newWep.transform.parent = inventory.weaponSlot[0].transform;
+        //         int id = hit.transform.GetComponent<ItemID>().itemID;
+
+        //         if (database.weapons[id].weaponType == 1) {
+        //             if (inventory.inventory[0] == id) {
+        //                 Debug.Log("You already have this weapon.");
+        //             } else if (inventory.inventory[0] != id) {
+        //                 Destroy(primaryWep.gameObject);
+        //                 inventory.inventory[0] = id;
+        //                 primaryWep = Instantiate(database.weapons[id].weaponObj, inventory.weaponSlot[0].gameObject.transform.position, inventory.weaponSlot[0].gameObject.transform.rotation);
+        //                 primaryWep.transform.parent = inventory.weaponSlot[0].transform;
+        //             } else if (inventory.inventory[0] == 0) {
+        //                 inventory.inventory[0] = id;
+        //                 primaryWep = Instantiate(database.weapons[id].weaponObj, inventory.weaponSlot[0].gameObject.transform.position, inventory.weaponSlot[0].gameObject.transform.rotation);
+        //                 primaryWep.transform.parent = inventory.weaponSlot[0].transform;
+        //             }
+        //         }
+        //         if (database.weapons[id].weaponType == 2) {
+        //             if (inventory.inventory[1] == id) {
+        //                 Debug.Log("You already have this weapon.");
+        //             } else if (inventory.inventory[1] != id) {
+        //                 Destroy(secondaryWep.gameObject);
+        //                 inventory.inventory[1] = id;
+        //                 secondaryWep = Instantiate(database.weapons[id].weaponObj, inventory.weaponSlot[1].gameObject.transform.position, inventory.weaponSlot[1].gameObject.transform.rotation);
+        //                 secondaryWep.transform.parent = inventory.weaponSlot[1].transform;
+        //             } else if (inventory.inventory[1] == 0) {
+        //                 inventory.inventory[1] = id;
+        //                 secondaryWep = Instantiate(database.weapons[id].weaponObj, inventory.weaponSlot[1 ].gameObject.transform.position, inventory.weaponSlot[1].gameObject.transform.rotation);
+        //                 secondaryWep.transform.parent = inventory.weaponSlot[1].transform;
+        //             }
+        //         }
+            }
+        }
+    }
+
+    void CreateGun() {
+        
+    }
+}
--- capstone_project/Assets/Scripts/PlayerController.cs
+++ capstone_project/Assets/Scripts/PlayerController.cs
@@ -11,53 +11,44 @@ public class PlayerController : MonoBehaviour {
     public Vector3 jump;
     public float jumpForce = 2.0f;
     public bool isGrounded;
-    private bool isCrouching = false;
     private int count;
     public Text countText;
     public Text winText;
     GameObject pUp;
-    float crouchHeight;
-    float standHeight;
-    private PickupController pc;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        pc = GetComponent<PickupController>();
         count = 0;
         SetCountText();
         winText.text = "";
         pUp = GameObject.Find("Pickup");
         jump = new Vector3(0.0f, 2.0f, 0.0f);
-        standHeight = rb.position.y;
-        crouchHeight = standHeight/2;
     }
 
     void Update() {
         float moveHor = Input.GetAxis("Horizontal");
         float moveVer = Input.GetAxis("Vertical");
         moveDirection = (moveHor * transform.right + moveVer * transform.forward).normalized;
-        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+        if ( Input.GetKeyDown(KeyCode.LeftShift)) {
             walkSpeed *= 2;
         }
-        else if(Input.GetKeyUp(KeyCode.LeftShift)) {
+        else if( Input.GetKeyUp(KeyCode.LeftShift)) {
             walkSpeed /= 2;
         }
         else if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
-        } else if (Input.GetKeyDown(KeyCode.C)) {
-            isCrouching = !isCrouching;
         }
         
     }
     void FixedUpdate()
     {
         Move();
-        // if (DetectCollision()) {
-        //     pUp.SetActive(false);
-        //     count += 1;
-        //     SetCountText();
-        // }
+        if (DetectCollision()) {
+            pUp.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
     }
 
     // Collision Detection just using positions
@@ -79,18 +70,13 @@ public class PlayerController : MonoBehaviour {
 
     // Collision Detection using Collisions/Triggers
 
-    void OnTriggerEnter(Collider other) {
-        // if (other.gameObject.CompareTag("Pickup")) {
-        //     other.gameObject.SetActive(false);
-        //     count += 1;
-        //     SetCountText();
-        // }
-
-        if (other.gameObject.CompareTag("Pickup")) {
-            Debug.Log("Weapon/Player Collision");
-            // pc.PickupWeapon(other.gameObject);
-        }
-    }
+    // void OnTriggerEnter(Collider other) {
+    //     if (other.gameObject.CompareTag("Pickup")) {
+    //         other.gameObject.SetActive(false);
+    //         count += 1;
+    //         SetCountText();
+    //     }
+    // }
 
     void OnCollisionStay() {
         isGrounded = true;
@@ -101,18 +87,5 @@ public class PlayerController : MonoBehaviour {
         if (count >= 1) {
             winText.text = "You Win!";
         }
-    }
-
-    void Crouch() {
-        if (isCrouching) {
-            Vector3 tempPos = rb.position;
-            Vector3 newPos = new Vector3(tempPos.x, standHeight, tempPos.z);
-            rb.position = newPos;
-        } else {
-            Vector3 tempPos = rb.position;
-            Vector3 newPos = new Vector3(tempPos.x, crouchHeight, tempPos.z);
-            rb.position = newPos;
-        }
-        
     }
 }
