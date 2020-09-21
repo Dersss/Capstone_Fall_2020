78a357a7900a48cd97658a4f2660a7a8663ed3eb
Linda K <lindakohlndorfer@gmail.com>
2020-09-21 14:05:32 -0400

    attacking zombie now heads towards the player and attacks when close enough

--- capstone_project/Assets/Scripts/EnemyAI.cs
+++ capstone_project/Assets/Scripts/EnemyAI.cs
@@ -11,24 +11,41 @@ public class EnemyAI : MonoBehaviour
     void Start()
     {
         animator = this.GetComponent<Animator>();
-        
+        animator.SetBool("atPlayer", false);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
+        Vector3 player = new Vector3(GameObject.Find("PlayerCollider").transform.position.x, 0, GameObject.Find("PlayerCollider").transform.position.z);
+        transform.LookAt(player);
+
+
+
      
         
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Bullet"))
+        /*if (other.gameObject.CompareTag("Bullet"))
         {
             //fall back animation and despawn
 
             
+        }*/
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("atPlayer", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("atPlayer", false);
         }
     }
 }
---
+++ capstone_project/Assets/Scripts/ZAttack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Nothing useful is happening here yet
+public class ZAttack : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        animator.SetBool("atPlayer", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //heads toward player
+        Vector3 player = new Vector3(GameObject.Find("PlayerCollider").transform.position.x, 0, GameObject.Find("PlayerCollider").transform.position.z);
+        transform.LookAt(player);
+
+
+
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if (other.gameObject.CompareTag("Bullet"))
+        {
+            //fall back animation and despawn
+
+            
+        }*/
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("atPlayer", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("atPlayer", false);
+        }
+    }
+}
